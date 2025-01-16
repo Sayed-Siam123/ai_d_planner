b238c7922c44bae23c02153cc1b5918f2ea7157d
sayedsiam <msiam18@gmail.com>
2025-01-16 17:16:55 +0600

    hot fix

--- lib/app/modules/splash/views/splash_view.dart
+++ lib/app/modules/splash/views/splash_view.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
 
 import 'package:ai_d_planner/app/core/style/app_style.dart';
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../core/base/base_view.dart';
 import '../../../core/style/app_colors.dart';
@@ -28,8 +30,27 @@ class SplashPage extends BaseView {
     // TODO: implement body
     return Column(
       children: [
-        Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-          style: textRegularStyle(context,fontWeight: FontWeight.normal,languageSelect: LanguageSelect.gilroy,fontSize: 20),),
+        Expanded(
+          child: Swiper(
+            indicatorLayout: PageIndicatorLayout.SLIDE,
+            itemBuilder: (BuildContext context,int index){
+              return ClipRRect(borderRadius: BorderRadius.circular(30),child: Image.network("https://via.assets.so/album.png?id=${index+1}&q=95&w=360&h=360&fit=fill",fit: BoxFit.fill,));
+            },
+            itemCount: 10,
+            // pagination: SwiperPagination(),
+            // control: SwiperControl(),
+            layout: SwiperLayout.STACK,
+            itemWidth: 300,
+            itemHeight: 300,
+            axisDirection: AxisDirection.right,
+            allowImplicitScrolling: true,
+            curve: Curves.easeInOut,
+            outer: false,
+          ),
+        ),
+
+        // Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        //   style: textRegularStyle(context,fontWeight: FontWeight.normal,languageSelect: LanguageSelect.gilroy,fontSize: 20),),
       ],
     );
   }
